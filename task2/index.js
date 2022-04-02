83f5904f7994c2921bc489724af9982ad5259b98
katiakovalchuk <kate.kovalchuk225@gmail.com>
2022-04-02 18:11:20 +0300

    solve task2

--- task2/index.js
+++ task2/index.js
@@ -1,4 +1,20 @@
-function calculate() {
-  //Here is your solution
-  return
+
+
+function grid(N) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+    if (N < 0) return null;
+
+    const rows = [];
+    for (let i = 0; i < N; i++){
+        const row = [];
+        for (let j = 0; j < N; j++){
+            const idx = (i + j) % alphabet.length;
+            row.push(alphabet[idx]);
+        }
+        rows.push(row.join(' '));
+    }
+
+    return rows.join('\n')
 }
+
