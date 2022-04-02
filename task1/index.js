8e28a18e026952e1a83e6fdfb8a37224212c13cb
katiakovalchuk <kate.kovalchuk225@gmail.com>
2022-04-02 18:09:58 +0300

    solve task1

--- task1/index.js
+++ task1/index.js
@@ -1,4 +1,14 @@
-function calculate() {
-  //Here is your solution
-  return
+
+
+function calculate(equation) {
+    const [a, operator, b] = equation.split(' ');
+    const operators = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b,
+        '*': (a, b) => a * b,
+        '//': (a, b) => Math.floor(a / b)
+    }
+    const dotsNumber = operators[operator](a.length, b.length);
+
+    return ''.padStart(dotsNumber, '.');
 }
